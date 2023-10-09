c32e2ba113fb82a1a237a293fb296fb6d5a8bd51
Dr. Gabor FACSKO <59848559+gfacsko@users.noreply.github.com>
2023-10-09 02:38:01 +0200

    Nappalis HelloWorld

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ *  HelloWord first app
+ *  
+ *  Developed by Gabor FACSKO (facsko.gabor@uni-milton.hu)
+ *  Milton Friedman University, Budapest, Hungary, 2023
+ * 
+ */
+//Console.WriteLine("Hello, World!"); // Kiírás
+//Console.WriteLine("Szia Világ!");
+//Console.WriteLine("Szia Világ!");
+
+//Console.Write("Szia Világ!");
+//Console.Write("Szia Világ!");
+
+/*
+ * Console.Write("Írjon be egy szöveget! ");
+string str = Console.ReadLine();
+
+Console.WriteLine("A beolvasott szöveg: " + str);
+*/
+
+/*
+int a = 5;
+
+double b = 6.6;
+
+string str = "Valami";
+
+bool l = true;
+
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("str = '" + str + "'");
+Console.WriteLine("l = " + l);
+
+//float c = (float)b;
+float c = (float)a;
+
+Console.WriteLine("c = " + c);
+*/
+
+Console.WriteLine("aaa");
+Console.WriteLine("aaa\tbbb");
+Console.WriteLine("aaa\n\n");
+Console.WriteLine("---");
+
+//Environment.NewLine = "\r\n";
+
+Console.ReadKey();
